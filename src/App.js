0eda9cbaa450f738f3927614cd8f5202cb51dd91
khris <khris56@hotmail.com>
2019-07-29 19:43:30 -0500

    characters counter implemented

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      form: ""
+    }
+  }
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" value={this.state.form} onChange={this.updateForm.bind(this)}>form</textarea>
+        <div className="counter">{this.state.form.length}</div>
       </div>
     );
+  }
+
+  updateForm(event){
+    this.setState({
+      form: event.target.value
+    })
   }
 }
 
